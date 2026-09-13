8bb16d064a6a3dc129338e498f1a462e4c5c1978
dostrakon1 <227500497+dostrakon1@users.noreply.github.com>
2026-09-13 02:24:28 +0200

    Route Sinterklaas to premium seasonal page

--- src/app/collecties/[slug]/page.tsx
+++ src/app/collecties/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { ChristmasCollectionPage } from '@/components/storefront/christmas-collection-page'
 import { SeasonalCollectionPage } from '@/components/storefront/seasonal-collection-page'
+import { SinterklaasCollectionPage } from '@/components/storefront/sinterklaas-collection-page'
 import { Breadcrumbs, EditorialIntro, EditorialShell } from '@/components/storefront/editorial-shell'
 import { categories, getCategoryContent } from '@/content/categories'
 import { getCategoryImage } from '@/content/category-images'
@@ -66,6 +67,9 @@ export default async function CollectionPage({ params }: { params: Promise<{ slu
   if (seasonalCampaign) {
     if (collection.slug === 'kerst') {
       return <ChristmasCollectionPage collection={collection} campaign={seasonalCampaign} />
+    }
+    if (collection.slug === 'sinterklaas') {
+      return <SinterklaasCollectionPage collection={collection} campaign={seasonalCampaign} />
     }
     return <SeasonalCollectionPage collection={collection} campaign={seasonalCampaign} />
   }
